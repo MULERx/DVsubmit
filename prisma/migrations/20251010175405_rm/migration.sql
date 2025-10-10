7cadfde85a5e58fcbe08fc9580e04b821761821e
Mulugeta <xmullerx123@gmail.com>
2025-10-10 20:55:19 +0300

    im

---
+++ prisma/migrations/20251010175405_rm/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the `retention_policies` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "public"."retention_policies";
