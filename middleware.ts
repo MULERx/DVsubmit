a634293e03fd8e01c245958448f899fdd3cf352c
Mulugeta <xmullerx123@gmail.com>
2025-10-15 11:33:42 +0300

    im

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
-import { type NextRequest } from 'next/server'
-import { updateSession } from './src/lib/supabase/middleware'
+import { type NextRequest } from "next/server";
+import { updateSession } from "./src/lib/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+  return await updateSession(request);
 }
 
 export const config = {
@@ -12,8 +12,7 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
-}
+};
